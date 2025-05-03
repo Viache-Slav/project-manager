a964b23f8cc75f6ed645e6dcc98e3672344e440d
Viache-Slav <slavikfedko@gmail.com>
2025-05-03 18:14:48 +0200

    fix: use FRONTEND_URL for redirect after Google login/logout

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -14,7 +14,7 @@ router.get('/google',
 router.get('/google/callback',
   passport.authenticate('google', { failureRedirect: '/' }),
   (req, res) => {
-    res.redirect('http://localhost:5173/dashboard');
+    res.redirect(`${process.env.FRONTEND_URL}/dashboard`);
   }
 );
 
@@ -23,7 +23,7 @@ router.get('/logout', (req, res) => {
     if (err) { return next(err); }
 
     req.session.destroy(() => {
-      res.redirect('http://localhost:5173/');
+      res.redirect(`${process.env.FRONTEND_URL}/`);
     });
   });
 });
